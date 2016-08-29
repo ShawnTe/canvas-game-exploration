f29d8ae42287a5aae8f8ac3f5915d26bc2d93c22
Hunter Chapman <bootcoder@gmail.com>
2016-08-28 17:40:21 -0700

    Change ball color when ball hits top or bottom

--- application.js
+++ application.js
@@ -14,10 +14,10 @@ var x = canvas.width * .5;
 var y = canvas.height - 30;
 var dx = -1;
 var dy = -2;
+var color = "blue"
 
 function drawBall(color) {
   var ballRadius = 10;
-
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI*2);
   ctx.fillStyle = color
@@ -27,31 +27,24 @@ function drawBall(color) {
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  // var color
-  // console.log(color)
-  var color = "blue" || newColor
-  // color += "#"
   drawBall(color);
   x += dx;
   y += dy;
   // subtract the ballRadius if we want change direction to happen on contact instead of at center of ball
   if (y + dy < 0 || y + dy > canvas.height) {
     dy = -dy;
-    var newColor = changeColor();
-    console.log(color)
-
+    color = changeColor();
   }
   if (x + dx < 0 || x + dx > canvas.width) {
     dx = -dx;
   }
-
 }
 
 function changeColor(){
   var colors = ["#75bba0", "#ad382b", "#386133"]
   var num = Math.floor(Math.random() * colors.length);
-  var color = colors[num]
-  return color
+  var newColor = colors[num]
+  return newColor
 }
 setInterval(draw, 10);
 
